d1bc0aec0c9a99d3f429197b3d13d796c4688c75
Eloghene <eloghene@bananacrystal.com>
2025-04-26 14:59:44 +0100

    Return role with user details on log in

--- app/serializers/sephcocco_user_serializer.rb
+++ app/serializers/sephcocco_user_serializer.rb
@@ -1,5 +1,5 @@
 class SephcoccoUserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :address, :phone_number, :whatsapp_number, :created_at, :updated_at
+  attributes :id, :name, :email, :address, :phone_number, :whatsapp_number, :role, :created_at, :updated_at
 
   def role
     object.sephcocco_user_role.name if object.sephcocco_user_role
