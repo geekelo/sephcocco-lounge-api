087d09e50b553695d1c54c8beee89848facaa784
Eloghene <eloghene@bananacrystal.com>
2025-05-19 13:34:36 +0100

    Fix bug caused by wrong syntac in product model

--- app/models/sephcocco_lounge_product_category.rb
+++ app/models/sephcocco_lounge_product_category.rb
@@ -2,7 +2,7 @@ class SephcoccoLoungeProductCategory < ApplicationRecord
   has_and_belongs_to_many :sephcocco_lounge_products, join_table: 'sephcocco_lounge_product_categories_products', foreign_key: :sephcocco_lounge_product_category_id, association_foreign_key: :sephcocco_lounge_product_id
 
   before_create :create_slug
-  before_update :create_slug, if: :name_changes?
+  before_update :create_slug, if: :saved_change_to_name?
 
   private
 
