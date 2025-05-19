98e345fcaa6602ba9350a3e7a753a6f6926c1a5b
Eloghene <eloghene@bananacrystal.com>
2025-05-19 15:27:38 +0100

    Fix bugs

--- db/migrate/20250519092135_create_join_table_products_categories.rb
+++ db/migrate/20250519092135_create_join_table_products_categories.rb
@@ -1,5 +1,5 @@
 class CreateJoinTableProductsCategories < ActiveRecord::Migration[7.2]
-  def change
+  def up
     create_table :sephcocco_lounge_product_categories_products, id: false do |t|
       t.uuid :sephcocco_lounge_product_id, null: false
       t.uuid :sephcocco_lounge_product_category_id, null: false
@@ -9,5 +9,10 @@ class CreateJoinTableProductsCategories < ActiveRecord::Migration[7.2]
               [:sephcocco_lounge_product_id, :sephcocco_lounge_product_category_id], 
               unique: true, 
               name: 'index_products_categories_on_product_and_category'
+  end
+
+  def down
+    remove_index :sephcocco_lounge_product_categories_products, name: 'index_products_categories_on_product_and_category'
+    drop_table :sephcocco_lounge_product_categories_products
   end
 end
