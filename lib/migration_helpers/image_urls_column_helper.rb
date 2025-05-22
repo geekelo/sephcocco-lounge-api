f11d88792a6229d31c18ab787fd9df11017c320b
Eloghene <eloghene@bananacrystal.com>
2025-05-22 10:48:42 +0100

    Return if table exists

--- lib/migration_helpers/image_urls_column_helper.rb
+++ lib/migration_helpers/image_urls_column_helper.rb
@@ -1,11 +1,13 @@
 # lib/migration_helpers/image_urls_column_helper.rb
 
-module ImageUrlsColumnHelper
-  def add_image_urls_column(table_name)
-    add_column table_name.to_sym, :image_urls, :string, array: true, default: []
-  end
+module MigrationHelpers
+  module ImageUrlsColumnHelper
+    def add_image_urls_column(table_name)
+      add_column table_name.to_sym, :image_urls, :string, array: true, default: []
+    end
 
-  def remove_image_urls_column(table_name)
-    remove_column table_name.to_sym, :image_urls
+    def remove_image_urls_column(table_name)
+      remove_column table_name.to_sym, :image_urls
+    end
   end
 end
