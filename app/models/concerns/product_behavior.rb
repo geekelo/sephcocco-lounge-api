324e8e16c6cd053cdd022bcd06532fe82556558f
Eloghene <eloghene@bananacrystal.com>
2025-05-22 13:43:57 +0100

    keep model dry

--- app/models/concerns/product_behavior.rb
+++ app/models/concerns/product_behavior.rb
@@ -1,28 +1,17 @@
-# app/models/concerns/product_behavior.rb
 module ProductBehavior
   extend ActiveSupport::Concern
 
   included do
-    has_and_belongs_to_many category_association_name, {
-      join_table: category_join_table,
-      foreign_key: product_foreign_key,
-      association_foreign_key: category_foreign_key
-    }
+    has_and_belongs_to_many category_association_name,
+                            join_table: join_table_name,
+                            foreign_key: product_foreign_key,
+                            association_foreign_key: category_foreign_key
 
-    has_many :product_likes_association_name, {
-      class_name: product_like_class_name,
-      foreign_key: product_foreign_key,
-      dependent: :destroy
-    }
+    has_many :product_likes_class, foreign_key: product_foreign_key, dependent: :destroy
+    has_many :likers, through: :product_likes_class, source: :sephcocco_user
 
-    has_many :likers, through: :product_likes_association_name, source: :sephcocco_user
-
-    has_many :orders_association_name, {
-      class_name: order_class_name,
-      foreign_key: product_foreign_key
-    }
-
-    has_many :buyers, through: :orders_association_name, source: :sephcocco_user
+    has_many :orders_class, foreign_key: product_foreign_key
+    has_many :buyers, through: :orders_class, source: :sephcocco_user
   end
 
   class_methods do
@@ -30,8 +19,8 @@ module ProductBehavior
       raise NotImplementedError, "Define self.category_association_name in your model"
     end
 
-    def category_join_table
-      raise NotImplementedError, "Define self.category_join_table in your model"
+    def join_table_name
+      raise NotImplementedError, "Define self.join_table_name in your model"
     end
 
     def product_foreign_key
@@ -42,20 +31,12 @@ module ProductBehavior
       raise NotImplementedError, "Define self.category_foreign_key in your model"
     end
 
-    def product_likes_association_name
-      raise NotImplementedError, "Define self.product_likes_association_name in your model"
+    def product_likes_class
+      raise NotImplementedError, "Define self.product_likes_class in your model"
     end
 
-    def product_like_class_name
-      raise NotImplementedError, "Define self.product_like_class_name in your model"
-    end
-
-    def orders_association_name
-      raise NotImplementedError, "Define self.orders_association_name in your model"
-    end
-
-    def order_class_name
-      raise NotImplementedError, "Define self.order_class_name in your model"
+    def orders_class
+      raise NotImplementedError, "Define self.orders_class in your model"
     end
   end
 end
